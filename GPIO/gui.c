a273f927e71b2f0cccdbf460d1683b6e85d6c73f
Dima <trolzface@gmail.com>
2016-10-17 19:19:23 +0000

    no loop

--- GPIO/gui.c
+++ GPIO/gui.c
@@ -11,32 +11,12 @@ int main (void) {
     int col;
     int szx,szy;
     int i;
-    //char buf[32];
     char buf[15];
-    /*
-    const char *gpiowinlist[7];
-    gpiowinlist[0] = "gpio0";
-    gpiowinlist[1] = "gpio1";
-    gpiowinlist[2] = "gpio2";
-    gpiowinlist[3] = "gpio3";
-    gpiowinlist[4] = "gpio4";
-    gpiowinlist[5] = "gpio5";
-    gpiowinlist[6] = "gpio6";
-    gpiowinlist[7] = "gpio7";
-    */
     WINDOW *gpiowinlist[8];
 
-    //int      width = 23, height = 7;
-    //int      rows  = 25, cols   = 80;
-    //int      x = (cols - width)  / 2;
-    //int      y = (rows - height) / 2;
-
-
-
-
     if ( (mainwin = initscr()) == NULL) {
-    fprintf(stderr, "Error initializing ncurses.\n");
-    exit(EXIT_FAILURE);
+        fprintf(stderr, "Error initializing ncurses.\n");
+        exit(EXIT_FAILURE);
     }
     //getmaxyx(stdscr, sizew, sizeh);
     getmaxyx(stdscr, szy, szx);
@@ -53,25 +33,34 @@ int main (void) {
     noecho();
     keypad(mainwin, TRUE);
 
-    //sizew=10;
-
     gpiowinlist[0] = subwin(mainwin, sizeh, sizew, y, x);
+    gpiowinlist[1] = subwin(mainwin, sizeh, sizew, y, x + sizew);
+    gpiowinlist[2] = subwin(mainwin, sizeh, sizew, y, x + sizew*2);
+    gpiowinlist[3] = subwin(mainwin, sizeh, sizew, y, x + sizew*3);
+    gpiowinlist[4] = subwin(mainwin, sizeh, sizew, y + sizeh, x);
+    gpiowinlist[5] = subwin(mainwin, sizeh, sizew, y + sizeh, x + sizew);
+    gpiowinlist[6] = subwin(mainwin, sizeh, sizew, y + sizeh, x + sizew*2);
+    gpiowinlist[7] = subwin(mainwin, sizeh, sizew, y + sizeh, x + sizew*3);
     box(mainwin, '+','+');
     box(gpiowinlist[0], 0, 0);
+    box(gpiowinlist[1], 0, 0);
+    box(gpiowinlist[2], 0, 0);
+    box(gpiowinlist[3], 0, 0);
+    box(gpiowinlist[4], 0, 0);
+    box(gpiowinlist[5], 0, 0);
+    box(gpiowinlist[6], 0, 0);
+    box(gpiowinlist[7], 0, 0);
     mvwaddstr(gpiowinlist[0], 1, 4, "lololololol");
     mvwaddstr(gpiowinlist[0], (sizeh) / 2, (sizew) / 2, buf);
     //mvwaddstr(gpiowinlist[0], (height) / 2, (width) / 2, buf);
-    for (i = 1; i++; i < 5) {
-        gpiowinlist[i] = subwin(mainwin, sizeh, sizew, y, x + sizew*2);
+    /*for (i = 1; i++; i <= 3) {
+        gpiowinlist[i] = subwin(mainwin, sizeh, sizew, y, x + sizew); //- sizew);//*(i+1)-1);
         box(gpiowinlist[i], 0, 0);
-        //mvwaddstr(gpiowinlist[i], 1, 4, "lololololol");
+        snprintf(buf, sizeof(buf), "%d", i);
+        mvwaddstr(gpiowinlist[i], sizeh / 2, sizew / 2, buf);
+        mvwaddstr(gpiowinlist[i], 1, 4, "lololololol");
         refresh();
-    }
-
-    //mvwaddstr(gpiowin, 1, 4, "Move the window");
-    //mvwaddstr(gpiowin, 2, 2, "with the arrow keys");
-    //mvwaddstr(gpiowin, 3, 6, "or HOME/END");
-    //mvwaddstr(gpiowin, 5, 3, "Press 'q' to quit");
+    }*/
 
     refresh();
 
@@ -79,16 +68,26 @@ int main (void) {
 
     while ( (ch = getch()) != 'q' ) {
 
-        switch (ch) {
-        case KEY_RIGHT:
-            break;
-        case KEY_LEFT:
-            break;
-        //break;
-        }
+    //    switch (ch) {
+    //    case KEY_RIGHT:
+    //        break;
+    //    case KEY_LEFT:
+    //        break;
+    //    break;
+    //    clrscr();
+    //    }
     }
     delwin(gpiowinlist[0]);
     delwin(gpiowinlist[1]);
+    delwin(gpiowinlist[2]);
+    delwin(gpiowinlist[3]);
+    delwin(gpiowinlist[4]);
+    delwin(gpiowinlist[5]);
+    delwin(gpiowinlist[6]);
+    delwin(gpiowinlist[7]);
+   /* for (i = 1; i++; i < 4) {
+        delwin(gpiowinlist[i]);
+    }*/
     delwin(mainwin);
     endwin();
     refresh();
